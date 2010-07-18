7b74ac23d955d440896663f9ccf6be40b8c8fa03
Gopalakrishna Palem <Gopalakrishna.Palem@Yahoo.com>
2010-07-18 05:32:19 +0000

    Modified PHTRANSLATELIB_API definition to suit external applications
    git-svn-id: https://phtranslator.svn.sourceforge.net/svnroot/phtranslator@24 ae4679e1-54ca-4c1c-a4f8-39c64a239ce2

--- PhTranslateLib/PhTranslateLib.h
+++ PhTranslateLib/PhTranslateLib.h
@@ -4,10 +4,12 @@
 // that uses this DLL. This way any other project whose source files include this file see 
 // PHTRANSLATELIB_API functions as being imported from a DLL, whereas this DLL sees symbols
 // defined with this macro as being exported.
+#ifndef PHTRANSLATELIB_API
 #ifdef PHTRANSLATELIB_EXPORTS
 #define PHTRANSLATELIB_API __declspec(dllexport)
 #else
 #define PHTRANSLATELIB_API __declspec(dllimport)
+#endif
 #endif
 
 #include <string>
